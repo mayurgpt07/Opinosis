c3c7ef6c014ae8ad184f04695b7577af03d712d0
mayurgpt07 <mayurgpt07@gmail.com>
2020-10-13 23:17:11 -0700

    Added POS Tagger

--- CreateOpinionGraph.py
+++ CreateOpinionGraph.py
@@ -2,20 +2,25 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import re
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+
+fileName = "./opinosis-summarization/sample_data/kindle.txt" 
 
 '''
 Read the reviews to create two variables that can be used to create the graph and most occuring word dictionary
 Graph contains all the words, digits and characters
 Most frequently occuring words requires only words
-
 '''
-reviews = open("./opinosis-summarization/sample_data/toyota_camry.txt","r")
+
+reviews = open(fileName,"r")
 allText = reviews.read()
 
 # Remove next line character and breaklines fro reviews
 combinedSentences = allText.lower().strip().replace('\n', ' ').replace('\r', '')
+
 # Remove all uncessary characters and digits
 combinedSentences = re.sub("[^a-zA-Z]", " ", combinedSentences)
 combinedSentences = combinedSentences.strip()
@@ -44,7 +49,7 @@ Create a directed graph for the vocabulary in reviews
 Directed graphs allow traversal that supports the language used to write the review
 
 '''
-reviews = open("./opinosis-summarization/sample_data/toyota_camry.txt","r")
+reviews = open(fileName,"r")
 allText = reviews.read()
 allSentence = allText.lower().split('\n')
 
@@ -53,8 +58,10 @@ G = nx.DiGraph()
 G.add_nodes_from(uniqueWords)
 
 # Add weights to each edge, based on the occurance of relationship between words (i -> i+1)
+pos_tag_sentence = []
 weight_dict = {}
 for sentence in allSentence:
+    pos_tag_sentence.append(pos_tag(word_tokenize(sentence.lower().strip().replace('\n', ' ').replace('\r', ''))))
     allWordsinSentence = sentence.lower().strip().replace('\n', ' ').replace('\r', '').split(' ')
     for i in range(len(allWordsinSentence)-1):
         tup = (allWordsinSentence[i], allWordsinSentence[i+1])
@@ -65,6 +72,8 @@ for sentence in allSentence:
             weight_dict[tup] = 1
 
 reviews.close()
+
+print(pos_tag_sentence[0])
 keys = list(weight_dict.keys())
 
 # Set the weights to edges between words
@@ -73,11 +82,10 @@ for key in keys:
 
 
 '''
-
 Use TF-IDF to find the most important set of words in the reviews
 
 '''
-reviews = open("./opinosis-summarization/sample_data/toyota_camry.txt","r")
+reviews = open(fileName,"r")
 eachReview = reviews.readlines()
 
 # Clean reviews before running Tf-Idf
@@ -98,16 +106,15 @@ tfidf_sorting = np.argsort(tfidfVector.toarray()).flatten()[::-1]
 # Get the top n words based on descending order of tfidf score
 n = 10
 top_n = feature_array[tfidf_sorting][:n]
-
 print(top_n)
 
 '''
-
 Create summary reviews using graph and TF-IDF
 Select the starting word (must be in vocabulary) and the length of reviews
 The review either is of selected length or until it encounters a full stop (.)
 
 '''
+
 initialNode = top_n[9]
 maxWeight = -1
 node = ''
@@ -127,4 +134,3 @@ while lengthOfReviews > 0 and node != '.':
     lengthOfReviews = lengthOfReviews - 1
 
 print('Final Summary: ', finalString)
-
