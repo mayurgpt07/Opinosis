fdf3023c81707f803053bde4e15beef4d60e3af6
mayurgpt07 <mayurgpt07@gmail.com>
2020-10-15 15:39:44 -0700

    Added Average Position| Reviews look better

--- CreateOpinionGraph.py
+++ CreateOpinionGraph.py
@@ -7,12 +7,58 @@ from nltk import pos_tag
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 
-fileName = "./opinosis-summarization/sample_data/kindle.txt" 
+## Creating function to get the average starting position of word
+def average(avgList):
+    return len(avgList), sum(avgList)/len(avgList)
+
+def getAveragePosition(uniqueWords, allSentence):
+    startingWordList = uniqueWords
+    eachWordAvg = []
+    wordAveragePosition = {}
+    wordNumber = {}
+    for eachWord in startingWordList:
+        for eachSentence in allSentence:
+            index = eachSentence.lower().strip().replace('\n', ' ').replace('\r', '').find(eachWord)
+            if index != -1:
+                eachWordAvg.append(index + 1)
+        wordNumber[eachWord], wordAveragePosition[eachWord] = average(eachWordAvg)
+        eachWordAvg.clear()
+    return wordNumber, wordAveragePosition
+
+
+fileName = "./opinosis-summarization/sample_data/toyota_camry.txt"
 
 ## Get the main points using nouns
 ## Filter starting position list using pos
 ## Try to derive paths that must go through a word (and then find the most weighted amongst it)
 ## Use opionosis method as well
+## Linking Verbs (is, are etc) are hubs - Collapsible node
+## Cordinating Conjunction, punctuations () are ends
+
+
+contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not",
+                           "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not",
+                           "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",
+                           "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would",
+                           "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would",
+                           "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam",
+                           "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have",
+                           "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock",
+                           "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have",
+                           "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is",
+                           "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as",
+                           "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would",
+                           "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have",
+                           "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have",
+                           "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are",
+                           "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",
+                           "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is",
+                           "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have",
+                           "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have",
+                           "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all",
+                           "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have",
+                           "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
+                           "you're": "you are", "you've": "you have"}
 
 '''
 Read the reviews to create two variables that can be used to create the graph and most occuring word dictionary
@@ -32,6 +78,9 @@ combinedSentences = combinedSentences.strip()
 # print(combinedSentences)
 # Create another copy of initial review set for the graph
 allText = allText.lower().strip().replace('\n', ' ').replace('\r', '')
+allText = ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in allText.split(" ")]) 
+# print(allText)
+
 uniqueWords = list(set(allText.split(' ')))
 reviews.close()
 
@@ -58,28 +107,18 @@ reviews = open(fileName,"r")
 allText = reviews.read()
 allSentence = allText.lower().split('\n')
 
-## Creating function to get the average starting position of word
-def average(avgList):
-    return sum(avgList)/len(avgList)
+resolveShortForm = lambda x: ' '.join([contraction_mapping[t] if t in contraction_mapping else t for t in x.split(" ")])
+allSentence = list(map(resolveShortForm, allSentence))
+wordNumber, wordAveragePosition = getAveragePosition(uniqueWords, allSentence)
 
-def getAveragePosition(sorted_word_dictionary, allSentence):
-    startingWordList = list(sorted_word_dictionary.keys())
-    eachWordAvg = []
-    wordAveragePosition = {}
-    for eachWord in startingWordList:
-        for eachSentence in allSentence:
-            index = eachSentence.lower().strip().replace('\n', ' ').replace('\r', '').find(eachWord)
-            if index != -1:
-                eachWordAvg.append(index + 1)
-        wordAveragePosition[eachWord] = average(eachWordAvg)
-        eachWordAvg.clear()
-    return wordAveragePosition
-      
-wordAveragePosition = getAveragePosition(sorted_word_dictionary, allSentence)
+
 wordAveragePosition = {k: v for k, v in sorted(wordAveragePosition.items(), key=lambda item: item[1])}
-# print(wordAveragePosition)
+print(wordAveragePosition)
+
 # Create directed graph
 G = nx.DiGraph()
+
+## Add pos tagged nodes as the root nodes
 G.add_nodes_from(uniqueWords)
 
 # Add weights to each edge, based on the occurance of relationship between words (i -> i+1)
@@ -118,7 +157,7 @@ removeNewLine = lambda x: x.lower().strip().replace('\n', ' ').replace('\r', '')
 eachReview = list(map(removeNewLine, eachReview))
 
 # Do not remove any accent from the words, otherwise the words won't match the graph
-vectorizer = TfidfVectorizer(stop_words = 'english', sublinear_tf = True)
+vectorizer = TfidfVectorizer(stop_words = 'english', sublinear_tf = False)
 tfidfVector = vectorizer.fit_transform(eachReview)
 
 tfidf = tfidfVector.todense()
@@ -126,17 +165,21 @@ tfidf[tfidf == 0] = np.nan
 means = np.nanmean(tfidf, axis=0)
 means = dict(zip(vectorizer.get_feature_names(), means.tolist()[0]))
 
-tfidf = tfidfVector.todense()
-ordered = np.argsort(tfidf*-1)
-words = vectorizer.get_feature_names()
 
-global_average_score = {}
-top_k = 200
-for i, doc in enumerate(eachReview):
-    result = { }
-    for t in range(top_k):
-        result[words[ordered[i,t]]] = means[words[ordered[i,t]]]
-        
+sorted_means = {k: v for k, v in sorted(means.items(), key=lambda item: item[1])}
+# print(sorted_means)
+# tfidf = tfidfVector.todense()
+# ordered = np.argsort(tfidf*-1)
+# words = vectorizer.get_feature_names()
+
+
+# global_average_score = {}
+# top_k = 5
+# for i, doc in enumerate(eachReview):
+#     result = {}
+#     for t in range(top_k):
+#         result[words[ordered[i,t]]] = means[words[ordered[i,t]]]
+
 
 '''
 Create summary reviews using graph and TF-IDF
@@ -145,14 +188,15 @@ The review either is of selected length or until it encounters a full stop (.)
 
 '''
 
-initialNode = 'orientation'#top_n[9]
+initialNode = 'appears'#top_n[9]
+# print(wordNumber[initialNode])
 maxWeight = -1
 node = ''
 finalString = initialNode
 lengthOfReviews = 10
 
 # Create summary review using max weight graph traversal (most common words coming after each other) 
-while lengthOfReviews > 0 and node != '.':
+while lengthOfReviews > 0 and (node != '.' and node != '!'):
     for neighbor in G.neighbors(initialNode):
         weight = G[initialNode][neighbor]['weight']
         if weight > maxWeight:
